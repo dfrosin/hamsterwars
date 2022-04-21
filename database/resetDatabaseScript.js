504886ae270bcb2a285745d9c6447762a1d2cf0d
David Forsyth Rosin <drosin96@gmail.com>
2022-04-21 17:46:04 +0200

    add some progress messages to resetDatabaseScript.js

--- database/resetDatabaseScript.js
+++ database/resetDatabaseScript.js
@@ -5,13 +5,22 @@ const require = createRequire(import.meta.url)
 const freshHamsters = require('../data.json')
 const colRef = collection(db, 'hamsters')
 
-let n = 1
+console.log('resetting...')
+
 const snapshot = await getDocs(colRef)
 snapshot.docs.forEach(async (snapshot) => {
   const docRef = doc(colRef, snapshot.id)
   await deleteDoc(docRef)
 })
+
+let isFinished = false
+let n = 1
 freshHamsters.forEach(async (data) => {
+  if (!isFinished) {
+    console.log('\x1b[32m', '--- DELETED ALL DOCS ---')
+    console.log('\x1b[0m', 'adding new docs from JSON file...')
+  }
+  isFinished = true
   await addDoc(colRef, data)
   n++
   if (n === freshHamsters.length) {
